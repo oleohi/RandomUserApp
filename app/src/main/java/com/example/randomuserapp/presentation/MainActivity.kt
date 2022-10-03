dc32172c5488e0e4c88296d8970afbeba5efe7ed
olohialli <oleohi.alli@fcmb.com>
2022-10-03 05:48:51 +0100

    project set up and gradle dependencies resolution

--- app/src/main/java/com/example/randomuserapp/presentation/MainActivity.kt
+++ app/src/main/java/com/example/randomuserapp/presentation/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.randomuserapp
+package com.example.randomuserapp.presentation
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -10,7 +10,7 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.randomuserapp.ui.theme.RandomUserAppTheme
+import com.example.randomuserapp.presentation.ui.theme.RandomUserAppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -31,7 +31,7 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+    Text(text = "Hello $name!", style = MaterialTheme.typography.h1)
 }
 
 @Preview(showBackground = true)
---
+++ app/src/main/java/com/example/randomuserapp/presentation/ui/theme/Color.kt
@@ -0,0 +1,10 @@
+package com.example.randomuserapp.presentation.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple200 = Color(0xFFBB86FC)
+val Purple500 = Color(0xFF6200EE)
+val Purple700 = Color(0xFF3700B3)
+val Teal200 = Color(0xFF03DAC5)
+val Blackish = Color(0xFF212121)
+val TextWhite = Color(0xFFEEEEEE)
